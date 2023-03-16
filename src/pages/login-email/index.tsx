8c92a33e753f8d75abcc1c0228f414df58bd3000
Christian-pprogrammer <mpanoc6@gmail.com>
2023-03-16 10:47:25 -0300

    finish ui for login flow

---
+++ src/pages/login-email/index.tsx
@@ -0,0 +1,229 @@
+// ** ReactImports
+import { ChangeEvent, FormEvent, FormEventHandler, MouseEvent, ReactNode, useState } from 'react'
+
+// ** Next Import
+import Link from 'next/link'
+
+// ** MUI Components
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Divider from '@mui/material/Divider'
+import Checkbox from '@mui/material/Checkbox'
+import TextField from '@mui/material/TextField'
+import InputLabel from '@mui/material/InputLabel'
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
+import CardContent from '@mui/material/CardContent'
+import FormControl from '@mui/material/FormControl'
+import OutlinedInput from '@mui/material/OutlinedInput'
+import { styled, useTheme } from '@mui/material/styles'
+import MuiCard, { CardProps } from '@mui/material/Card'
+import InputAdornment from '@mui/material/InputAdornment'
+import MuiFormControlLabel, { FormControlLabelProps } from '@mui/material/FormControlLabel'
+
+// ** Icon Imports
+import Icon from 'src/@core/components/icon'
+
+// ** Configs
+import themeConfig from 'src/configs/themeConfig'
+
+// ** Layout Import
+import BlankLayout from 'src/@core/layouts/BlankLayout'
+
+// ** Demo Imports
+import FooterIllustrationsV1 from 'src/views/pages/auth/FooterIllustrationsV1'
+import { useRouter } from 'next/router'
+interface State {
+  phoneOrEmail: string
+}
+
+// ** Styled Components
+const Card = styled(MuiCard)<CardProps>(({ theme }) => ({
+  [theme.breakpoints.up('sm')]: { width: 450 }
+}))
+
+const FormControlLabel = styled(MuiFormControlLabel)<FormControlLabelProps>(({ theme }) => ({
+  '& .MuiFormControlLabel-label': {
+    fontSize: '0.875rem',
+    color: theme.palette.text.secondary
+  }
+}))
+
+const LoginEmailPage = () => {
+  // ** State
+  const [values, setValues] = useState<State>({
+    phoneOrEmail: ''
+  })
+
+  // ** Hook
+  const theme = useTheme()
+
+  //router
+  const router = useRouter();
+
+  const handleChange = (prop: keyof State) => (event: ChangeEvent<HTMLInputElement>) => {
+    setValues({ ...values, [prop]: event.target.value })
+  }
+
+  const handleMouseDownPassword = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    router.push("/login-password");
+  }
+  
+  return (
+    <Box className='content-center'>
+      <Card sx={{zIndex: 1}}>
+        <CardContent sx={{ p: theme => `${theme.spacing(13, 7, 6.5)} !important` }}>
+          <Box sx={{ mb: 8, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <svg width={47} fill='none' height={26} viewBox='0 0 268 150' xmlns='http://www.w3.org/2000/svg'>
+              <rect
+                rx='25.1443'
+                width='50.2886'
+                height='143.953'
+                fill={theme.palette.primary.main}
+                transform='matrix(-0.865206 0.501417 0.498585 0.866841 195.571 0)'
+              />
+              <rect
+                rx='25.1443'
+                width='50.2886'
+                height='143.953'
+                fillOpacity='0.4'
+                fill='url(#paint0_linear_7821_79167)'
+                transform='matrix(-0.865206 0.501417 0.498585 0.866841 196.084 0)'
+              />
+              <rect
+                rx='25.1443'
+                width='50.2886'
+                height='143.953'
+                fill={theme.palette.primary.main}
+                transform='matrix(0.865206 0.501417 -0.498585 0.866841 173.147 0)'
+              />
+              <rect
+                rx='25.1443'
+                width='50.2886'
+                height='143.953'
+                fill={theme.palette.primary.main}
+                transform='matrix(-0.865206 0.501417 0.498585 0.866841 94.1973 0)'
+              />
+              <rect
+                rx='25.1443'
+                width='50.2886'
+                height='143.953'
+                fillOpacity='0.4'
+                fill='url(#paint1_linear_7821_79167)'
+                transform='matrix(-0.865206 0.501417 0.498585 0.866841 94.1973 0)'
+              />
+              <rect
+                rx='25.1443'
+                width='50.2886'
+                height='143.953'
+                fill={theme.palette.primary.main}
+                transform='matrix(0.865206 0.501417 -0.498585 0.866841 71.7728 0)'
+              />
+              <defs>
+                <linearGradient
+                  y1='0'
+                  x1='25.1443'
+                  x2='25.1443'
+                  y2='143.953'
+                  id='paint0_linear_7821_79167'
+                  gradientUnits='userSpaceOnUse'
+                >
+                  <stop />
+                  <stop offset='1' stopOpacity='0' />
+                </linearGradient>
+                <linearGradient
+                  y1='0'
+                  x1='25.1443'
+                  x2='25.1443'
+                  y2='143.953'
+                  id='paint1_linear_7821_79167'
+                  gradientUnits='userSpaceOnUse'
+                >
+                  <stop />
+                  <stop offset='1' stopOpacity='0' />
+                </linearGradient>
+              </defs>
+            </svg>
+            <Typography variant='h6' sx={{ ml: 2, lineHeight: 1, fontWeight: 700, fontSize: '1.5rem !important' }}>
+              {themeConfig.templateName}
+            </Typography>
+          </Box>
+          <Box sx={{ mb: 6 }}>
+            <Typography variant='h5' sx={{ mb: 1.5, fontWeight: 600, letterSpacing: '0.18px' }}>
+              {`Sign in`}
+            </Typography>
+            <Typography variant='body2'>Please sign-in to your Zyco account</Typography>
+          </Box>
+          <form noValidate autoComplete='off' onSubmit={handleSubmit}>
+            <TextField autoFocus fullWidth id='phoneOrEmail' label='Phone or email' sx={{ mb: 4 }} />
+            {/* <FormControl fullWidth>
+              <InputLabel htmlFor='auth-login-password'>Password</InputLabel>
+              <OutlinedInput
+                label='Password'
+                value={values.password}
+                id='auth-login-password'
+                onChange={handleChange('password')}
+                type={values.showPassword ? 'text' : 'password'}
+                endAdornment={
+                  <InputAdornment position='end'>
+                    <IconButton
+                      edge='end'
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      aria-label='toggle password visibility'
+                    >
+                      <Icon icon={values.showPassword ? 'mdi:eye-outline' : 'mdi:eye-off-outline'} />
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl> */}
+            <Box
+              sx={{ mb: 4, display: 'flex', alignItems: 'center', flexWrap: 'wrap', justifyContent: 'space-between' }}
+            >
+              <FormControlLabel
+                label='Remember Me'
+                control={<Checkbox />}
+                sx={{ '& .MuiFormControlLabel-label': { color: 'text.primary' } }}
+              />
+              <Typography
+                variant='body2'
+                component={Link}
+                href='/pages/auth/forgot-password-v1'
+                sx={{ color: 'primary.main', textDecoration: 'none' }}
+              >
+                Can't login
+              </Typography>
+            </Box>
+            <Button fullWidth size='large' type='submit' variant='contained' sx={{ mb: 7 }}>
+              NEXT
+            </Button>
+            <Box sx={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap', justifyContent: 'center' }}>
+              <Typography sx={{ mr: 2, color: 'text.secondary' }}>New on our platform?</Typography>
+              <Typography
+                component={Link}
+                href='/pages/auth/register-v1'
+                sx={{ color: 'primary.main', textDecoration: 'none' }}
+              >
+                Create an account
+              </Typography>
+            </Box>
+            
+          </form>
+        </CardContent>
+      </Card>
+      <FooterIllustrationsV1 />
+    </Box>
+  )
+}
+
+LoginEmailPage.getLayout = (page: ReactNode) => <BlankLayout>{page}</BlankLayout>
+
+LoginEmailPage.guestGuard = true
+
+export default LoginEmailPage
